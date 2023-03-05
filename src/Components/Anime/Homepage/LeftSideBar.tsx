65ff3c1169a41d531204304a8cbe6628370486b9
Sebastian <sebcyde@hotmail.com>
2023-03-05 13:09:35 +0000

    added in left side anime section extender

--- src/Components/Anime/Homepage/LeftSideBar.tsx
+++ src/Components/Anime/Homepage/LeftSideBar.tsx
@@ -1,5 +1,7 @@
 import { AnimeListType, AnimeType } from '../../../Types/AnimeTypes';
-import React from 'react';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import React, { useState } from 'react';
 import LeftSideCard from './LeftSideCard';
 
 type Props = {
@@ -8,12 +10,24 @@ type Props = {
 };
 
 const LeftSideBar = ({ AnimeList, Title }: Props) => {
+	const [Extend, setExtend] = useState(false);
+
 	return (
 		<div className="LeftSideBarContainer">
 			<h2 className="LeftSidebarTitle">{Title}</h2>
-			{AnimeList.slice(0, 10).map((Anime: AnimeType, index: number) => {
-				return <LeftSideCard Anime={Anime} Rank={index + 1} />;
-			})}
+			{AnimeList.slice(0, Extend ? 10 : 5).map(
+				(Anime: AnimeType, index: number) => {
+					return <LeftSideCard Anime={Anime} Rank={index + 1} />;
+				}
+			)}
+
+			<p className="ExtendArrow" onClick={() => setExtend(!Extend)}>
+				{Extend ? (
+					<KeyboardDoubleArrowUpIcon />
+				) : (
+					<KeyboardDoubleArrowDownIcon />
+				)}
+			</p>
 		</div>
 	);
 };
